9faf3cad94c9464619ef54d0d12407a23c03d8b8
Helder Matos <heldermiguelmm@hotmail.com>
2017-07-13 23:38:15 +0100

    int to css

--- takemetochurch/src/main/java/client/Navigation.java
+++ takemetochurch/src/main/java/client/Navigation.java
@@ -86,6 +86,7 @@ public class Navigation {
 
     public void setScene(Scene scene) {
         stage.setScene(scene);
+        scene.getStylesheets().add("/stylesheet/styles.css");
         stage.setResizable(false);
         stage.show();
     }
